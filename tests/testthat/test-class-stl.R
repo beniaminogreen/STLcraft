c975258cc84a4ec7083b9a3ab747c65e09f76201
Beniamino Green <ben@greendalba.com>
2019-11-17 19:54:16 +0000

    moved write_stl

--- tests/testthat/test-class-stl.R
+++ tests/testthat/test-class-stl.R
@@ -1,6 +1,7 @@
 context('class STL tests')
 test_that('SetValidity works for STLs', {
 	expect_error(STL(volcano, volcano, 1), 'the size must be specified in 3 dimensions')
+	expect_error(STL(volcano, volcano +1), 'Bottom surface may never be above top surface')
 	expect_error(STL(volcano, matrix()), 'Top and Bottom surface must have same size')
 	expect_error(STL(matrix(c('a','b','c')),matrix(c(1,2,3))), 'Top surface should be a numeric matrix')
 	expect_error(STL(matrix(c(1,2,3)),matrix(c('a','b','c'))), 'Bottom surface should be a numeric matrix')
