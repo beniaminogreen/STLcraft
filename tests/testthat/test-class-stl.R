86622a3736ed6d23aaa61fc57dfdaadc24691f59
Beniamino Green <ben@greendalba.com>
2020-02-13 21:53:08 +0000

    final test tweaks for the night

--- tests/testthat/test-class-stl.R
+++ tests/testthat/test-class-stl.R
@@ -9,9 +9,6 @@ test_that('SetValidity works for STLs', {
 	expect_error(STL(volcano, color = c('a','b')), 'color must be a string')
 })
 test_that('STL nrow/ncol/dim methods work', {
-	stl_1 <- STL(volcano)
-	expect_equal(nrow(STL(volcano)), nrow(volcano))
-	expect_equal(ncol(STL(volcano)), ncol(volcano))
 	expect_equal(dim(STL(volcano)), dim(volcano))
 })
 test_that('show_stl works',{
